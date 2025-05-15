ca50acffcb810cf574a6c11f572b4ee02bffac14
CrossShot06 <sohamrs0610@gmail.com>
2025-05-15 18:22:36 -0400

    added new templates

---
+++ accounts/urls.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.home),
+    path('user/',views.user),
+    path('trainer/',views.trainer)
+]
--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def home(request):
+    return render(request,"accounts/home.html")
+
+
+def user(request):
+    return render(request,"accounts/user.html")
+
+def trainer(request):
+    return render(request,"accounts/user.html")
+
+
