39461e84e39ee4d34105988da2aa25aa5d0605d8
CrossShot06 <sohamrs0610@gmail.com>
2025-05-20 17:52:23 -0400

    completed role based access

--- accounts/urls.py
+++ accounts/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     path('trainer/',views.trainer,name="trainer"),
     path('register/',views.register,name="register"),
     path('login/',views.login_view,name="login"),
+    path('logout/',views.logout_view,name='logout')
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,17 +1,33 @@
 from django.shortcuts import render, redirect
 from .forms import CreateUserForm
 from .models import Profile
-from django.contrib.auth.models import User
+from .decorators import unautheticated_user,allowed_users,admin_only
+from django.contrib.auth.models import Group
 from django.contrib.auth import authenticate , login , logout
+from django.contrib.auth.decorators import login_required
+from django.contrib import messages
+
+
+@login_required(login_url='login')
+@admin_only
 def home(request):
     return render(request, "accounts/home.html")
 
+
+@login_required(login_url='login')
+@allowed_users(allowed_roles=['admin','user'])
 def user(request):
     return render(request, "accounts/user.html")
 
+
+
+@login_required(login_url='login')
+@allowed_users(allowed_roles=['admin','trainer'])
 def trainer(request):
     return render(request, "accounts/trainer.html")
 
+
+@unautheticated_user
 def register(request):
     form = CreateUserForm()
 
@@ -21,14 +37,18 @@ def register(request):
             user = form.save()
             role = form.cleaned_data.get('role')
 
-            # Create profile with selected role
             Profile.objects.create(user=user, role=role)
+
+            group = Group.objects.get(name=role)
+            user.groups.add(group)
 
             return redirect('login')
 
     context = {'form': form}
     return render(request, "accounts/register.html", context)
 
+
+@unautheticated_user
 def login_view(request):
 
     if request.method == 'POST':
@@ -39,6 +59,11 @@ def login_view(request):
         if user is not None:
             login(request,user)
             return redirect('home')
-
+        else:
+            messages.info(request,'Username Or Password is Incorrect')
     context={}
     return render(request,'accounts/login.html',context)
+
+def logout_view(request):
+    logout(request)
+    return redirect('login')
